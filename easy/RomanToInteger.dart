04b5862a703dea1a21d2fcf5fc1414c2270032f5
Bhavneet Singh <bhavneet0812@gmail.com>
2025-07-06 18:28:28 +0530

    Reorganize main function to follow class definition in RomanToInteger.dart

--- easy/RomanToInteger.dart
+++ easy/RomanToInteger.dart
@@ -1,7 +1,3 @@
-void main() {
-  final solution = Solution();
-}
-
 class Solution {
   int romanToInt(String s) {
     int result = 0;
@@ -44,4 +40,33 @@ class Solution {
         throw ArgumentError('Invalid Roman numeral symbol');
     }
   }
+}
+
+void main() {
+  final solution = Solution();
+
+  // Example 1
+  String s1 = "III";
+  int result1 = solution.romanToInt(s1);
+  print(result1); // Output: 3
+
+  // Example 2
+  String s2 = "IV";
+  int result2 = solution.romanToInt(s2);
+  print(result2); // Output: 4
+
+  // Example 3
+  String s3 = "IX";
+  int result3 = solution.romanToInt(s3);
+  print(result3); // Output: 9
+
+  // Example 4
+  String s4 = "LVIII";
+  int result4 = solution.romanToInt(s4);
+  print(result4); // Output: 58
+
+  // Example 5
+  String s5 = "MCMXCIV";
+  int result5 = solution.romanToInt(s5);
+  print(result5); // Output: 1994
 }
